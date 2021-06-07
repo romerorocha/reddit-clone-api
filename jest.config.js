584e2a01c609f10d984a2565caf8092921d07444
Claudio Romero <25067140+romerorocha@users.noreply.github.com>
2021-06-07 11:43:33 -0300

    Atualiza config coverage

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,5 @@
 module.exports = {
   preset: "ts-jest",
   testEnvironment: "node",
-  collectCoverageFrom: ["src/{!(index),}/{!(docs), }.ts"],
+  collectCoverageFrom: ["src/{!(index),}/{!(*Controller),}.ts"],
 };
