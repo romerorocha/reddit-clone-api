3920228a067d9299786a4ec21e5563b00511011a
Claudio Romero <25067140+romerorocha@users.noreply.github.com>
2021-09-21 09:47:23 -0300

    Add mais dados de posts

--- src/post/PostRepository.ts
+++ src/post/PostRepository.ts
@@ -35,6 +35,61 @@ const init: Posts = {
     nota: 5,
     numeroComentarios: 0,
   },
+  "cfa609dc-1ad2-11ec-9621-0242ac130002": {
+    id: "cfa609dc-1ad2-11ec-9621-0242ac130002",
+    timestamp: 1632225380870,
+    titulo: "React Hooks vs. React HOCs",
+    corpo:
+      'O primeiro é um recurso que nós podemos utilizar exclusivamente com componentes do tipo "função". O segundo, é um padrão que aplicamos para com qualquer componente. Mas, mesmo assim, prefiro os hooks!',
+    autor: "Aluno de opinião forte",
+    categoria: "react",
+    nota: 7,
+    numeroComentarios: 0,
+  },
+  "bf977ae8-1ad3-11ec-9621-0242ac130002": {
+    id: "bf977ae8-1ad3-11ec-9621-0242ac130002",
+    timestamp: 1632225712638,
+    titulo: "Gerenciamento de estado em React",
+    corpo:
+      'Sinceramente, espero que chegue logo no módulo sobre Redux. Não aguento mais ficar declarando estado em componente "pai" ou "avô", só para compartilhar em um passa e repassa de props.',
+    autor: "Aluno mimizento",
+    categoria: "react",
+    nota: -2,
+    numeroComentarios: 0,
+  },
+  "269b9fc6-1ad4-11ec-9621-0242ac130002": {
+    id: "269b9fc6-1ad4-11ec-9621-0242ac130002",
+    timestamp: 1632225890371,
+    titulo: "React e a imutabilidade",
+    corpo:
+      "Ainda tenho algumas dúvidas sobre o assunto e, às vezes, me confundo e meto a mão diretamente no objeto do estado. Mas, aos poucos, tem entrado na minha cabeça, essa coisa de imutabilidade. Não é que faz sentido? Começando a costar. Por sinal, alguém aí tem um material bom, para aprofundar no assunto?",
+    autor: "Aluna dedicada",
+    categoria: "react",
+    nota: 58,
+    numeroComentarios: 0,
+  },
+  "eb1dbaaa-1ad4-11ec-9621-0242ac130002": {
+    id: "eb1dbaaa-1ad4-11ec-9621-0242ac130002",
+    timestamp: 1632226217345,
+    titulo: "Bibliotecas de componentes de UI para React",
+    corpo:
+      "Gostaria de fazer uma análise comparativa sobre bibliotecas de componentes de UI em React, como Material-UI, Semantic UI React, Bootstrap React e afins. Alguém aí sugere alguma outra, para também entrar na lista?",
+    autor: "Aluna blogueirinha",
+    categoria: "react",
+    nota: 4,
+    numeroComentarios: 0,
+  },
+  "9a0a3c1e-1ad5-11ec-9621-0242ac130002": {
+    id: "9a0a3c1e-1ad5-11ec-9621-0242ac130002",
+    timestamp: 1632226517463,
+    titulo: "Gerenciamento de forms em React",
+    corpo:
+      "Qual lib vocês recomendam, para trabalhar com forms em React? Muita gente fala de Formik, outros ainda colocam Formik + Yup, outros ainda preferem a lib React Hook Form. Já me disseram que redux-forms é um antipattern e deve ser evitado. Aceitando sugestões :)",
+    autor: "exploradora_de_libs",
+    categoria: "react",
+    nota: 6,
+    numeroComentarios: 0,
+  },
 };
 
 let posts = clone(init);
